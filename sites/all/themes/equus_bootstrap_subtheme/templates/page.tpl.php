ee5cd43f7c515d7f0aaeb68d7a238097fe8fd3d4
Christina Donald <christina-donald@hotmail.com>
2014-04-08 22:00:58 -0500

    Trying to re-design navbar

--- sites/all/themes/equus_bootstrap_subtheme/templates/page.tpl.php
+++ sites/all/themes/equus_bootstrap_subtheme/templates/page.tpl.php
@@ -73,18 +73,19 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+<header id="navbar" role="banner" class="primary-nav" style="display: inline block;">
   <div class="container">
     <div class="navbar-header">
+      <!--
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $base_path . drupal_get_path('theme', 'equus_bootstrap_subtheme'); ?>/Logo.svg" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
+      * <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+      * <img src="<?php print $base_path . drupal_get_path('theme', 'equus_bootstrap_subtheme'); ?>/Logo.svg" alt="<?php print t('Home'); ?>" />
+      * </a>
+      * <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
       <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+      <?php endif; ?>-->
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
