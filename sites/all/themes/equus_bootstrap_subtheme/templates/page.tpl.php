50c1e29960dfe61d014800cad4b547ba1f3a8170
Christina Donald <christina-donald@hotmail.com>
2014-04-12 15:28:10 -0500

    Primary Nav and Body
    Playing around with the classes, was able to expand the container.

--- sites/all/themes/equus_bootstrap_subtheme/templates/page.tpl.php
+++ sites/all/themes/equus_bootstrap_subtheme/templates/page.tpl.php
@@ -73,19 +73,18 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="primary-nav" style="display: inline block;">
+<header id="navbar" role="navigation" class="<?php print $navbar_classes; ?>">
   <div class="container">
     <div class="navbar-header">
-      <!--
       <?php if ($logo): ?>
-      * <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-      * <img src="<?php print $base_path . drupal_get_path('theme', 'equus_bootstrap_subtheme'); ?>/Logo.svg" alt="<?php print t('Home'); ?>" />
-      * </a>
-      * <?php endif; ?>
+      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
+      <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
       <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>-->
+      <?php endif; ?>
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
