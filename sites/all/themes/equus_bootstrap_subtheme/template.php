75218d0f535f783e2fbb63dc7dde36dc085c8a04
potetofurai <janellegithub@zaphoyd.com>
2014-06-11 20:37:34 -0500

    Removed text format menu from comment form

--- sites/all/themes/equus_bootstrap_subtheme/template.php
+++ sites/all/themes/equus_bootstrap_subtheme/template.php
@@ -4,3 +4,12 @@
  * @file
  * template.php
  */
+
+function equus_bootstrap_subtheme_form_comment_form_alter(&$form, &$form_state, &$form_id) {
+  $form['comment_body']['#after_build'][] = '_equus_bootstrap_subtheme_customize_comment_form';
+}
+
+function _equus_bootstrap_subtheme_customize_comment_form(&$form) {
+  $form[LANGUAGE_NONE][0]['format']['#access'] = FALSE;
+  return $form;
+}
