fb7b16e4f32de74dbe0ea409783faea38fdec394
eggchan <kaufmans@MIN7601w-kaufmansa-3.local>
2014-08-07 12:27:41 -0500

    update favorite icon

--- sites/all/themes/equus_bootstrap_subtheme/template.php
+++ sites/all/themes/equus_bootstrap_subtheme/template.php
@@ -146,7 +146,7 @@ function equus_bootstrap_subtheme_preprocess_node(&$vars) {
 }	
 
 function equus_bootstrap_subtheme_preprocess_flag(&$vars) {
-	$vars['link_text'] = "<3 ".equus_bootstrap_subtheme_get_like_count($vars['entity_id']);
+	$vars['link_text'] = equus_bootstrap_subtheme_get_like_count($vars['entity_id']);
 }
 
 function equus_bootstrap_subtheme_get_like_count($nid) {
