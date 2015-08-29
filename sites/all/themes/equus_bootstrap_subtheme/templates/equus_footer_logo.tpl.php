23849503ce44d628e0949f0eb6cd3c5bf15a91e9
Janelle Thorson <janellegithub@zaphoyd.com>
2015-08-29 19:31:14 -0400

    Calc ES Year and display in footer

--- sites/all/themes/equus_bootstrap_subtheme/templates/equus_footer_logo.tpl.php
+++ sites/all/themes/equus_bootstrap_subtheme/templates/equus_footer_logo.tpl.php
@@ -1,5 +1,5 @@
 <div class="footer-logo">
   <img src="<?php echo base_path() . path_to_theme() . "/images/es-device-white.svg"; ?>" class="logoImg" />
   <span class="footer-header">Equus-Sims</span>
-  <span class="footer-year">Summer, Year 8</span>
+  <span class="footer-year"><?php print equus_core_get_es_pretty_year(); ?></span>
 </div>
